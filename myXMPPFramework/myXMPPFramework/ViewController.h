297fdd0af51dcabdb2adab1b9b4e665927e1127f
yanxiang <yanxiang909@126.com>
2016-03-15 16:34:09 +0800

    创建工程

---
+++ myXMPPFramework/myXMPPFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myXMPPFramework
+//
+//  Created by 颜祥 on 16/3/15.
+//  Copyright © 2016年 yanxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myXMPPFramework/myXMPPFramework/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  myXMPPFramework
+//
+//  Created by 颜祥 on 16/3/15.
+//  Copyright © 2016年 yanxiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+
+@end
